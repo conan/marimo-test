7952c7dcb79f0e4f8900f9253d01f438ec6afe2e
Conan <cpc@paratusltd.com>
2025-09-24 15:59:04 +0100

    initial commit

---
+++ test.py
@@ -0,0 +1,38 @@
+import marimo
+
+__generated_with = "0.16.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    mo.md("# Welcome to marimo! 🌊🍃")
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    slider = mo.ui.slider(1, 22)
+    return (slider,)
+
+
+@app.cell
+def _(mo, slider):
+    mo.md(
+        f"""
+    marimo is a **reactive** Python notebook.
+
+    This means that unlike traditional notebooks, marimo notebooks **run
+    automatically** when you modify them or
+    interact with UI elements, like this slider: {slider}.
+
+    {"##" + "🍃" * slider.value}
+    """
+    )
+    return
+
+
+if __name__ == "__main__":
+    app.run()
